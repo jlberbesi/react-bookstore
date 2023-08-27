c7a8cc4aba0da05a9c5ef24421e81a65093d2610
Jlberbesi <jlberbesi.walloom@gmail.com>
2023-08-26 19:52:53 -0500

    Add initial components folder

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import Categories from './components/Categories';
+import BookList from './components/BookList';
+import BookForm from './components/BookForm';
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  const addBook = (newBook) => {
+    setBooks((prevBooks) => [...prevBooks, newBook]);
+  };
+
+  const deleteBook = (bookId) => {
+    setBooks((prevBooks) => prevBooks.filter((book) => book.id !== bookId));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+        <h2>Bookstore</h2>
+        <BookForm onAdd={addBook} />
+        <BookList books={books} onDelete={deleteBook} />
+      </div>
+    </Router>
   );
 }
 

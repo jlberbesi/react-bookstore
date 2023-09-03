8d651571d1247669148e8bd7ee0ee69273c24292
Jlberbesi <jlberbesi.walloom@gmail.com>
2023-09-02 23:55:59 -0500

    Add correction suggested by code reviewer

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -10,7 +10,7 @@ const categoriesSlice = createSlice({
   initialState,
   reducers: {
     getStatus: (state) => {
-      state.status = 'Under construction';
+      state.status = '';
     },
   },
 });
